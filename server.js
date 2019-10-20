d5533f194df332791ad629a96f4b06a123cec6dd
Luis ALfonso Marquez Pertuz <lmarqueza@uninorte.edu.co>
2019-10-20 17:02:27 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,156 @@
+//data base
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: 'momlamrds.c1f1b5fwsook.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'qwertyuiop',
+    database: 'momlam1'
+});
+//sniffer
+const dgram = require("dgram");
+const server = dgram.createSocket("udp4");
+var mensaje;
+server.on("error", err => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+server.on("message", (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  mensaje = msg.toString("utf8");
+
+  // Deco
+  let long, lat, fech;
+  long = mensaje.slice(27, 31) + mensaje.slice(31, 36);
+  long = parseFloat(long);
+  lat = mensaje.slice(19, 22) + mensaje.slice(22, 27);
+  lat = parseFloat(lat);
+  if (long > 0) {
+    if (long < 10) {
+      long = -70 - long;
+    }
+  }
+  if (long > 70) {
+    long = -1 * long;
+  }
+
+  console.log(lat);
+
+  fech = mensaje.slice(6, 19);
+  //d = datos[10];
+  // tiem = datos.slice(11, 16);
+  fech = new Date(parseFloat(fech) - 18000000); // Parses a string and returns a number
+  // console.log(fech);
+  let Fecha = `${fech.getFullYear()}-${fech.getMonth() + 1}-${fech.getDate()}`;
+  let Hora = `${fech.getHours()}:${fech.getMinutes()}:${fech.getSeconds()}`;
+  if (con) {
+    console.log("Connected!");
+    var sql =
+      "INSERT INTO syrusmsg (fecha , hora , Latitud , Longitud ) VALUES ?";
+    var value = [[Fecha, Hora, lat, long]];
+    con.query(sql, [value], function(err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      con.end();
+    });
+  } else {
+    console.log("Error conection with db");
+  }
+
+  // insertar aqui db
+});
+server.on("listening", () => {
+  const address = server.address();
+  //console.log(`server listening ${address.address}:${address.port}`);
+});
+server.bind(41500, "172.31.93.87"); //172.31.86.140
+// Prints: server listening 0.0.0.0:41234
+
+//base de datos
+
+// server
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+var hbs = require("express-hbs");
+const path = require("path");
+const port = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine(
+  "hbs",
+  hbs.express4({
+    partialsDir: __dirname + "/views"
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", __dirname + "/views");
+
+// static files
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "MOMLAM"
+  });
+});
+
+app.get("/coord", (req, res) => {
+  if (con) {
+    var sql = "SELECT * FROM syrusmsg ORDER BY igmsg DESC limit 1 ";
+    con.query(sql, function(err, result) {
+      if (err) throw err;
+      res.json(result[0]);
+      //con.end();
+    });
+  } else {
+    console.log("error conection with db");
+  }
+  //res.json(`${mensaje}`);
+});
+app.get("/coord1", (req, res) => {
+    if (con) {
+      var sql = "SELECT * FROM syrusmsg2 ORDER BY igmsg DESC limit 1 ";
+      con.query(sql, function(err, result) {
+        if (err) throw err;
+        res.json(result[0]);
+        //con.end();
+      });
+    } else {
+      console.log("error conection with db");
+    }
+    //res.json(`${mensaje}`);
+  });
+// app.get('/plana', (req, res) => {
+//     res.render('coor', {});
+// });
+app.post("/history", (req, res) => {
+  console.log(req.body); // Requiring data in historicos
+  if (con) {
+    console.log("Connected!");
+    var sql =
+      "SELECT * FROM syrusmsg where Fecha between ? and ? and Hora between ? and ? ";
+    var value = [
+      req.body.fecha1,
+      req.body.fecha2,
+      req.body.hora1,
+      req.body.hora2
+    ];
+    con.query(sql, value, function(err, result) {
+      if (err) throw err;
+      res.json(result);
+      //con.end();
+    });
+  } else {
+    console.log("Error conection with db");
+  }
+});
+app.get("/mapa", (req, res) => {
+  res.render("mapa", {});
+});
+app.get("/historicos", (req, res) => {
+  res.render("historicos", {});
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando peticiones en el puerto ${port}`);
+});
---
+++ sql.js
@@ -0,0 +1,48 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: 'momlamrds.c1f1b5fwsook.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'qwertyuiop',
+    database: 'momlam1'
+});
+
+// Crear base de datos 
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("CREATE DATABASE momlamdb", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
+
+//crear tabla
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE syrusmsg2 (igmsg INT,hora TIME(6),fecha DATETIME(6),longitud VARCHAR(255),latitud VARCHAR(255),RPM VARCHAR(255))";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
+
+//Insertar elemento
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "INSERT INTO syrusmsg (mensaje) VALUES ('LUCHO ES BOBO')";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("1 record inserted");
+//     });
+//   });
+// Borrar elemneto de la base de datos
+// con.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "DELETE FROM syrusmsg WHERE mensaje = 'LUCHO ES BOBO'";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Number of records deleted: " + result.affectedRows);
+//     });
+//   });
